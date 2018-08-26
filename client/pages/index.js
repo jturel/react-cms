400db062d1a327a354d1eada7f14e4c3d840e13f
Jonathon Turel <jturel@gmail.com>
2018-08-25 21:55:32 -0400

    eslint + fetch posts from rails

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,23 +1,29 @@
 import Layout from '../components/Layout'
 import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 
-const PostLink = (props) => (
-  <li>
-    <Link href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
-)
-
-const Index = () => (
+const Index = (props) => (
   <Layout>
     <h1>The Posts</h1>
     <ul>
-      <PostLink title="First one" />
-      <PostLink title="Second one" />
-      <PostLink title="Third one" />
+      {props.posts.map((post, i) => (
+        <li key={i}>
+          <Link href={`/post/${post.id}`}>
+            <a>{post.title}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
   </Layout>
 )
+
+Index.getInitialProps = async function() {
+  const res = await fetch('http://localhost:3001/api/v1/posts')
+  const data = await res.json()
+
+  return {
+    posts: data
+  }
+}
 
 export default Index
